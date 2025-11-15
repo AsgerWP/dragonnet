7349f016a1aca928f43c056d4a57e41aba61ea63
AsgerWP <lzt134@alumni.ku.dk>
2025-11-15 18:06:08 +0100

    refactor: inline methods

--- basic_neural_network/generate_data.py
+++ basic_neural_network/generate_data.py
@@ -15,21 +15,16 @@ def generate_data(n=1000, n_covariates=10, average_treatment_effect=3):
 
 def _generate_data(n=1000, n_covariates=10, average_treatment_effect=3):
     covariates = np.random.normal(0, 1, (n, n_covariates))
-    probits = _calculate_probits(covariates)
+    probits = covariates[:, 1] + covariates[:, 2] + covariates[:, 3]
     treatment_probabilities = 1 / (1 + np.exp(-probits))
     treatments = np.random.binomial(1, treatment_probabilities)
-    outcomes = _get_outcomes(covariates, average_treatment_effect, treatments) + np.random.normal(0, 1, n)
+    outcomes = (
+        average_treatment_effect * treatments
+        + covariates[:, 1]
+        + covariates[:, 2]
+        + covariates[:, 4]
+        + np.random.normal(0, 1, n)
+    )
     return covariates, treatments, outcomes
 
 
-def _calculate_probits(covariates):
-    return covariates[:, 1] + covariates[:, 2] + covariates[:, 3]
-
-
-def _get_outcomes(covariates, theta, average_treatment_effect):
-    return (
-        theta * average_treatment_effect
-        + np.sin(covariates[:, 1])
-        + np.cos(covariates[:, 2])
-        + covariates[:, 4]
-    )
