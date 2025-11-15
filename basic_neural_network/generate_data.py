e38a4196209357ca945676dee7737c94bf83d4fd
AsgerWP <lzt134@alumni.ku.dk>
2025-11-15 18:05:10 +0100

    feat: add noise to outcomes

--- basic_neural_network/generate_data.py
+++ basic_neural_network/generate_data.py
@@ -18,7 +18,7 @@ def _generate_data(n=1000, n_covariates=10, average_treatment_effect=3):
     probits = _calculate_probits(covariates)
     treatment_probabilities = 1 / (1 + np.exp(-probits))
     treatments = np.random.binomial(1, treatment_probabilities)
-    outcomes = _calculate_outcomes(covariates, average_treatment_effect, treatments)
+    outcomes = _get_outcomes(covariates, average_treatment_effect, treatments) + np.random.normal(0, 1, n)
     return covariates, treatments, outcomes
 
 
@@ -26,7 +26,7 @@ def _calculate_probits(covariates):
     return covariates[:, 1] + covariates[:, 2] + covariates[:, 3]
 
 
-def _calculate_outcomes(covariates, theta, average_treatment_effect):
+def _get_outcomes(covariates, theta, average_treatment_effect):
     return (
         theta * average_treatment_effect
         + np.sin(covariates[:, 1])
