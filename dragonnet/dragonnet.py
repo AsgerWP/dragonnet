47000f0159929630efe20caef1cc12be4151eb38
AsgerWP <lzt134@alumni.ku.dk>
2025-11-15 17:17:34 +0100

    feat: implement MLPNet

---
+++ dragonnet/dragonnet.py
@@ -0,0 +1,21 @@
+import torch
+import torch.nn as nn
+
+
+class MLPNet(nn.Module):
+    def __init__(self, covariate_size):
+        super(MLPNet, self).__init__()
+        self.first_layer = nn.Linear(covariate_size + 1, 64)
+        self.first_activation_layer = nn.ReLU()
+        self.second_layer = nn.Linear(64, 64)
+        self.second_activation_layer = nn.ReLU()
+        self.output_layer = nn.Linear(64, 1)
+        self.epsilon = nn.Parameter(torch.zeros(1))
+
+    def forward(self, _input):
+        hidden_state = self.first_layer(_input)
+        hidden_state = self.first_activation_layer(hidden_state)
+        hidden_state = self.second_layer(hidden_state)
+        hidden_state = self.second_activation_layer(hidden_state)
+        output = self.output_layer(hidden_state)
+        return output
